76553698182579fb1797c4276f218ca6a4bc3203
somukhan <mhsomrat154@gmail.com>
2023-03-03 16:13:40 +0600

    further modify build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "Building the project...."
-python -m pip install -r requirements.txt
-echo "Collecting staticfiles"
-python manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
