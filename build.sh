d8613b9f852da5d114f6fe27004f79a1daefe226
somukhan <mhsomrat154@gmail.com>
2023-03-03 16:10:19 +0600

    change build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Building the project...."
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 echo "Collecting staticfiles"
-python3.10 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
